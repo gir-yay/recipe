7c400c4f92b70373d8d9dd44e6fa924d1db3a402
ghizlane rahmouni <ghlaneiz022@gmail.com>
2024-10-26 22:05:25 +0100

    added a home controller

---
+++ src/main/java/com/app/recipe/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.app.recipe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping
+    public String homeController(){
+            return "Hello";
+    }
+}
