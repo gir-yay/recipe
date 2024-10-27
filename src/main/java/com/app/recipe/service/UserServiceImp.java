450d14b317d67b4ba11876e7d3afcbb5d23c4884
ghizlane rahmouni <ghlaneiz022@gmail.com>
2024-10-27 10:40:41 +0100

    bug fix: forget @service in UserServiceImp

--- src/main/java/com/app/recipe/service/UserServiceImp.java
+++ src/main/java/com/app/recipe/service/UserServiceImp.java
@@ -3,10 +3,13 @@ package com.app.recipe.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import com.app.recipe.model.User;
 import com.app.recipe.repository.UserRepository;
 
+@Service
 public class UserServiceImp implements UserService{
 
     @Autowired
